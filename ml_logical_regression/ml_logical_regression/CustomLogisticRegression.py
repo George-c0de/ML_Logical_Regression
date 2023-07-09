c53f08ae0a97047b50d68fa057a21284477adac6
GEORGE <gera.evseev2001@gmil.com>
2023-07-09 21:48:27 +0300

    Добален класс для инициализации логистической регрессии, усовершенствован метод преобразования категориального столбца skill_set в бинарный

--- ml_logical_regression/ml_logical_regression/CustomLogisticRegression.py
+++ ml_logical_regression/ml_logical_regression/CustomLogisticRegression.py
@@ -1,89 +1,324 @@
-import math
-import numpy as np
 import pandas as pd
-from sklearn.datasets import load_breast_cancer
+from pandas import DataFrame
+from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, log_loss
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+
+from ml_logical_regression.ml_logical_regression.decorator.decorators import measure_execution_time
 
 
-class CustomLogisticRegression:
+class LogicalRegression:
+    def __init__(self, file_name: str):
+        self.y_test = None
+        self.y_train = None
+        self.x_test = None
+        self.x_train = None
+        self.x_raw = None
+        self.y_raw = None
+        self.__file_name = file_name
+        self.data_raw: DataFrame | None = None
+        self.__tuple_names_to_binary = [
+            'city',
+            'specialty',
+            'gender',
+            'education_level',
+            'education_level',
+            'specialty',
+        ]
+        self._list_normalization_columes = [
+            'age',
+            'total_experience',
+        ]
+        self.model = LogisticRegression(fit_intercept=False, penalty='none')
+        self.threshold = 0.5
+        self.skills_for_save = [
+            'Python', 'Java', 'SQLite', 'Flask', 'Socket','async.io', 'JavaFX',
+        ]
+    @property
+    def return_columes(self) -> list[str]:
+        """
+        возвращает список столбцов
+        Returns: Список стоназваний столбцов
 
-    def __init__(self, fit_intercept=True, l_rate=0.01, n_epoch=100):
-        self.fit_intercept = fit_intercept
-        self.l_rate = l_rate
-        self.n_epoch = n_epoch
-        self.coef_ = np.array([[]])
+        """
+        return self.data_raw.columns.to_list()
 
-    def sigmoid(self, t):
-        return [1 / (1 + math.exp(-t[i])) for i in range(len(t))]
+    @property
+    def tuple_names_to_binary(self) -> list[str]:
+        """
+        Getter for tuple_names_to_binar
+        Returns:
+            Список элементов дял преобразования в бинарные признаки
+        """
+        return self.__tuple_names_to_binary
 
-    def predict_proba(self, row, coef_):
-        t = row @ coef_
-        return self.sigmoid(t)
+    @tuple_names_to_binary.setter
+    def tuple_names_to_binary(self, add_list: list[str], update: bool = True):
+        """
+        Обновляет или заменяет список
+        Args:
+            add_list: Список для обновления значения
+            update: True - Обновляет новым списком, False - заменяет на новый список
+        Returns:
 
-    def fit_mse(self, X_train, y_train):
-        n_row = np.shape(X_train)[0]
-        n_coefficient = np.shape(X_train)[1]
-        self.coef_ = np.zeros((n_coefficient, 1))
-        X_train = np.array(X_train)  # (n_row) x (n_feature)
-        y_train = np.array([y_train])  # (n_row) x (n_target)
+        """
+        if update:
+            self.__tuple_names_to_binary += add_list
+        else:
+            self.__tuple_names_to_binary = add_list
 
-        for iter_ in range(1, self.n_epoch + 1):
-            # if iter_ % 100 == 0:
-            #     print(f"\nIteration: {iter_}\n------------\n")
-            #     print(f"\nX_train: {X_train}\n")
-            #     print(f"\nCoefficients: {self.coef_}\n")
-            for i, row in enumerate(X_train):
-                row = np.array([row])  # 1 x (n_feature)
-                y_hat = self.predict_proba(row, self.coef_)  # 1x1
-                y_hat = np.array([y_hat])
-                # update all weights
-                self.coef_ = self.coef_ - self.l_rate * (y_hat - y_train[0, i]) * y_hat * (1 - y_hat) * row.T
+    @property
+    def data_raw(self) -> DataFrame:
+        """
+        Getter для получения необработанных данных
+        Returns:
+            Необработанные данные
+        """
+        return self._data_raw
 
-    def predict(self, X_test, cut_off=0.5):
-        n_row = np.shape(X_test)[0]
-        predictions = [0 for _ in range(n_row)]
-        X_test = np.array(X_test)
-        for i, row in enumerate(X_test):
-            y_hat = self.predict_proba(row, self.coef_)
-            y_hat = 1 if y_hat[0] >= cut_off else 0
-            predictions[i] = y_hat
-        return predictions  # predictions are binary values - 0 or 1
+    @data_raw.setter
+    def data_raw(self, value: DataFrame):
+        """
+        Setter для изменения необработанных данных
+        Args:
+            value: Данные для изменения
 
-    def fit_log_loss(self, X_train, y_train):
-        n_row = np.shape(X_train)[0]
-        n_coefficient = np.shape(X_train)[1]
-        self.coef_ = np.zeros((n_coefficient, 1))  # initialized weights and/or bias 1 x (n_coefficient)
-        X_train = np.array(X_train)  # (n_row) x (n_feature)
-        y_train = np.array([y_train])  # (n_row) x (n_target)
+        Returns:
 
-        for iter_ in range(1, self.n_epoch + 1):
-            # if iter_ % 100 == 0:
-            #     print(f"\nIteration: {iter_}\n------------\n")
-            #     print(f"\nX_train: {X_train}\n")
-            #     print(f"\nCoefficients: {self.coef_}\n")
-            for i, row in enumerate(X_train):
-                row = np.array([row])  # 1 x (n_feature)
-                y_hat = self.predict_proba(row, self.coef_)  # 1x1
-                y_hat = np.array([y_hat])
-                # update all weights
-                self.coef_ = self.coef_ - self.l_rate * ((y_hat - y_train[0, i]) / n_row) * row.T
+        """
+        self._data_raw = value
+
+    def load_data(self):
+        """
+        Загрузка данных и сохранение в переменной
+        Returns:
+            None
+        """
+        self.data_raw = pd.read_csv(self.__file_name, encoding='utf-8')
+        self.data_raw.drop(columns='Unnamed: 0', inplace=True)
+
+    @measure_execution_time
+    def process_city_column(self):
+        """
+        Преобразование категориального признака в бинарный
+        Returns:
+            Возвращает данные с бинарным признаком - ГОРОД
+        """
+
+        self.data_raw = pd.get_dummies(
+            self.data_raw,
+            columns=['city'],
+            dtype=int,  # Преобразование в 0 и 1
+            prefix='',
+            prefix_sep='',  # prefix = '',prefix_sep='', чтобы убрать переименования столбца
+        )
+
+    def process_education_column(self):
+        """
+        Преобразования столбца из категориального в бинарный признак - Образование
+        Returns:
+            Необработанные данные с бинарным признаком образования
+        """
+        self.data_raw = pd.get_dummies(
+            self.data_raw,
+            columns=['education_level'],
+            dtype=int,  # Преобразование в 0 и 1
+            prefix='',
+            prefix_sep='',  # prefix = '',prefix_sep='', чтобы убрать переименования столбца
+        )
+
+    def conversion_categorical_to_binary(self):
+        """
+        Преобразование категориальных признаков в бинарные
+        prefix = '',prefix_sep='', чтобы убрать переименования столбца
+        dtype = Преобразование в 0 и 1
+        Returns:
+
+        """
+        self.data_raw = pd.get_dummies(
+            self.data_raw,
+            columns=self.__tuple_names_to_binary,
+            dtype=int,
+            prefix='',
+            prefix_sep='',
+        )
+
+    def scaler_columes(self):
+        """
+        Нормализация данных
+        Returns: Возвращает необработанные данные с нормализацией
+
+        """
+        scaler = MinMaxScaler()
+        features = self.data_raw[self._list_normalization_columes]
+        normalized_features = scaler.fit_transform(features)
+        normalized_data = pd.DataFrame(normalized_features, columns=features.columns)
+        self.data_raw.update(normalized_data)
+
+    def create_data_for_learning(self):
+        """
+        Создание данных для разбивки на выборки
+        Returns:
+            Сохраняет данные для выборок
+        """
+        self.y_raw = self.data_raw.rating
+        self.x_raw = self.data_raw.loc[:, self.data_raw.columns != 'rating']
+
+    def create_train_split(self):
+        """
+        Проводит разбивку данных и сохраняет в переменные
+        Returns:
+            Сохраняет выборки в переменные класса
+        """
+
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
+            self.x_raw, self.y_raw, test_size=0.3, random_state=1000
+        )
+
+    def model_train(self):
+        """
+        Обучение модели
+        Returns:
+            Обученная модель сохраняется в переменную класса
+        """
+        self.model.fit(self.x_train, self.y_train)
+    def get_ru_columes(self):
+        result = []
+        for el in list(self.data_raw['skill_set']):
+            if not isinstance(el, str):
+                continue
+            for skill in el.split(','):
+                if (
+                        skill.upper() in result
+                        and all(ord(c) <= 127 for c in skill)
+                        and skill
+                ):
+                    result.append(skill.upper())
+    @measure_execution_time
+    def skills_to_categorical_volume(self):  # TODO Проверить метод
+        # Разделение столбца skill_set и создание нового столбца для каждого навыка
+        # Разбиение столбца skill_set на отдельные столбцы для каждого навыка
+        skills = self.data_raw['skill_set'].str.get_dummies(sep=',')
+
+        # Объединение полученных столбцов с исходным DataFrame
+        df = pd.concat([self.data_raw, skills], axis=1)
+
+        # Удаление столбца skill_set
+        df.drop('skill_set', axis=1, inplace=True)
+
+        print(df)
+        self.data_raw = df
+        return
+        skills.columns = skills.columns.map(lambda x: f'skill_{x + 1}')
+
+        # Заполнение столбцов новым столбцом и преобразование в 0 и 1
+        for col in skills.columns:
+            self.data_raw[col] = skills[col].apply(lambda x: 1 if pd.notnull(x) else 0)
+
+        # Заполнение отсутствующих навыков нулями
+        self.data_raw.fillna(0, inplace=True)
+
+        # Вывод DataFrame
+        print(self.data_raw)
+        return
+        skills_encoded = pd.get_dummies(self.data_raw['skill_set'], prefix='', prefix_sep='')
+
+        # Join the encoded skills with the original DataFrame
+        new_df = pd.concat([self.data_raw.drop('skill_set', axis=1), skills_encoded], axis=1)
+
+        # Print the new DataFrame
+        columes = new_df.columns
+        self.data_raw = pd.concat([self.data_raw, new_df], axis=1)
+        print(self.data_raw)
+        self.data_raw = pd.get_dummies(
+            self.data_raw,
+            columns=columes,
+            dtype=int,  # Преобразование в 0 и 1
+            prefix='',
+            prefix_sep='',  # prefix = '',prefix_sep='', чтобы убрать переименования столбца
+        )
+        print(self.data_raw)
+        return
+        result = []
+        for el in list(self.data_raw['skill_set']):
+            if not isinstance(el, str):
+                continue
+            for skill in el.split(','):
+                if (
+                        skill.upper() in result
+                        and all(ord(c) <= 127 for c in skill)
+                        and skill
+                ):
+                    result.append(skill.upper())
+
+        skills = self.data_raw["skill_set"].str.split(",").explode()
+
+        # Преобразование категориальных данных в бинарные столбцы
+        skills_encoded = pd.get_dummies(
+            skills,
+            dtype=int,
+            prefix='',
+            prefix_sep='',
+        )
+
+        # Отфильтровать преобразованные данные по списку result
+        skills_encoded = skills_encoded[skills_encoded.columns.intersection(result)]
+
+        # Сбросить индексы преобразованного датафрейма
+        skills_encoded = skills_encoded.reset_index(drop=True)
+
+        # Объединение преобразованных данных с исходным датафреймом
+        data_encoded = pd.concat([self.data_raw, skills_encoded], axis=1)
+
+        # Удаление исходного столбца "навыки"
+        data_encoded.drop("skill_set", axis=1, inplace=True)
+
+        # Вывод преобразованных данных
+        self.data_raw = data_encoded
 
 
-def standardize_z(x):
-    """
-    Take a nx1-vector, then perform z-standardization using:
-    "z[i] = (x[i] - mu) / sigma" where:
+    def delete_columes(self, columns_to_drop: list[str]):
+        """
 
-    - z[i]  --> i-th sample standard score
-    - x[i]  --> i-th sample value
-    - mu    --> mean of x
-    - sigma --> standard deviation of x
+        Args:
+            columns_to_drop:
 
-    :param x: feature data in dataframe (569 x 1 columns)
-              --> i.e. 'worst concave points', 'worst perimeter'
-    :return: standardized feature data in numpy array (569 x 1 columns)
-             --> i.e. 'std worst concave points', 'std worst perimeter'
-    """
-    z = list(map(lambda x_: (x_ - np.mean(x)) / np.std(x), x))
-    return z
+        Returns:
+
+        """
+        self.data_raw.drop(columns_to_drop, axis=1, inplace=True)
+
+    def transformation_y(self):
+        """
+        Проводит разбивку рейтинга, превращая данные в 0 и 1
+        Returns:
+            Обработанные данные рейтинга
+        """
+        self.data_raw['rating'] = self.data_raw['rating'].apply(lambda x: 1 if x > self.threshold else 0)
+
+    def model_fit(self):
+        """
+        Обучение модели
+        Returns:
+            Сохраняет обученную модель
+        """
+        self.model.fit(self.x_train, self.y_train)
+    def result(self):
+        y_pred = self.model.predict(self.x_raw)  # Результаты модели
+        print(confusion_matrix(self.y_raw, y_pred))
+        print(log_loss(y_pred, self.y_raw))
+        print('Б. Всей обучающей выборки')
+        y_pred = self.model.predict(self.x_train)
+        print(confusion_matrix(self.y_train, y_pred))
+        print(log_loss(y_pred, self.y_train))
+        print('В. Отобранных на собеседование кандидатов для обучающей выборки')
+        print(confusion_matrix(self.y_train, y_pred)[1][1])
+        print('Г.  Всей тестовой выборки')
+        y_pred = self.model.predict(self.x_test)
+        print(confusion_matrix(self.y_test, y_pred))
+        print(log_loss(y_pred, self.y_test))
+        print('Д. Отобранных на собеседование кандидатов для тестовой выборки')
+        print(confusion_matrix(self.y_test, y_pred)[1][1])
+
--- ml_logical_regression/ml_logical_regression/main.py
+++ ml_logical_regression/ml_logical_regression/main.py
@@ -1,191 +1,24 @@
-import pandas as pd
-from pandas import DataFrame
-from sklearn import preprocessing
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, log_loss
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
 
-from ml_logical_regression.ml_logical_regression.decorator.decorators import measure_execution_time
+from ml_logical_regression.ml_logical_regression.CustomLogisticRegression import LogicalRegression
 
+test = LogicalRegression('../../Data/dataNew.csv')  # Инициализация класса
 
-class LogicalRegression:
-    def __init__(self, file_name: str):
-        self.__file_name = file_name
-        self.data_raw: DataFrame | None = None
-        self.__tuple_names_to_binary = [
-            'city',
-            'specialty',
-            'gender',
-            'education_level',
-            'education_level',
-            'specialty',
-        ]
-        self._list_normalization_columes = [
-            'age',
-            'total_experience',
-        ]
-        self.model = LogisticRegression(fit_intercept=False, penalty='none')
-    @property
-    def return_columes(self) -> list[str]:
-        """
-        возвращает список столбцов
-        Returns: Список стоназваний столбцов
+test.load_data()  # Загрузка данных
 
-        """
-        return self.data_raw.columns.to_list()
+test.conversion_categorical_to_binary()  # Преобразование категориальных признаков в бинарные
 
-    @property
-    def tuple_names_to_binary(self) -> list[str]:
-        """
-        Getter for tuple_names_to_binar
-        Returns:
-            Список элементов дял преобразования в бинарные признаки
-        """
-        return self.__tuple_names_to_binary
+test.skills_to_categorical_volume()
+# test.delete_columes(columns_to_drop=['skill_set'])  # Удаление столбцов
 
-    @tuple_names_to_binary.setter
-    def tuple_names_to_binary(self, add_list: list[str], update: bool = True):
-        """
-        Обновляет или заменяет список
-        Args:
-            add_list: Список для обновления значения
-            update: True - Обновляет новым списком, False - заменяет на новый список
-        Returns:
+test.transformation_y()  # Преобразование рейтинга в 0 и 1
 
-        """
-        if update:
-            self.__tuple_names_to_binary += add_list
-        else:
-            self.__tuple_names_to_binary = add_list
+test.scaler_columes()  # нормализация данных
 
-    @property
-    def data_raw(self) -> DataFrame:
-        """
-        Getter для получения необработанных данных
-        Returns:
-            Необработанные данные
-        """
-        return self._data_raw
+test.create_data_for_learning()  # разделение данных на
 
-    @data_raw.setter
-    def data_raw(self, value: DataFrame):
-        """
-        Setter для изменения необработанных данных
-        Args:
-            value: Данные для изменения
+test.create_train_split()  # Создание выборок (тестовая, обучающая)
 
-        Returns:
+test.model_fit()  # Обучение модели
 
-        """
-        self._data_raw = value
-
-    def load_data(self):
-        """
-        Загрузка данных и сохранение в переменной
-        Returns:
-            None
-        """
-        self.data_raw = pd.read_csv(self.__file_name, encoding='utf-8')
-        self.data_raw.drop(columns='Unnamed: 0', inplace=True)
-
-    @measure_execution_time  # 0.055
-    def process_city_column(self):
-        """
-        Преобразование категориального признака в бинарный
-        Returns:
-            Возвращает данные с бинарным признаком - ГОРОД
-        """
-
-        self.data_raw = pd.get_dummies(
-            self.data_raw,
-            columns=['city'],
-            dtype=int,  # Преобразование в 0 и 1
-            prefix='',
-            prefix_sep='',  # prefix = '',prefix_sep='', чтобы убрать переименования столбца
-        )
-
-    def delete_columes(self, columes_for_delete: tuple):
-        """
-        Удаление столбцов
-        Args:
-            columes_for_delete: Кортеж названий столбцов
-        Returns:
-            None
-        """
-        self.data_raw.drop(columes_for_delete, inplace=True, axis=1)
-
-    def process_education_column(self):
-        """
-        Преобразования столбца из категориального в бинарный признак - Образование
-        Returns:
-            Необработанные данные с бинарным признаком образования
-        """
-        self.data_raw = pd.get_dummies(
-            self.data_raw,
-            columns=['education_level'],
-            dtype=int,  # Преобразование в 0 и 1
-            prefix='',
-            prefix_sep='',  # prefix = '',prefix_sep='', чтобы убрать переименования столбца
-        )
-
-    def conversion_categorical_to_binary(self):
-        """
-        Преобразование категориальных признаков в бинарные
-        prefix = '',prefix_sep='', чтобы убрать переименования столбца
-        dtype = Преобразование в 0 и 1
-        Returns:
-
-        """
-        self.data_raw = pd.get_dummies(
-            self.data_raw,
-            columns=self.__tuple_names_to_binary,
-            dtype=int,
-            prefix='',
-            prefix_sep='',
-        )
-
-    def scaler_columes(self):
-        scaler = MinMaxScaler()
-        features = self.data_raw[self._list_normalization_columes]
-        normalized_features = scaler.fit_transform(features)
-        normalized_data = pd.DataFrame(normalized_features, columns=features.columns)
-        self.data_raw.update(normalized_data)
-
-    def create_data_for_learning(self):
-        self.y_raw = self.data_raw.rating
-        self.x_raw = self.data_raw.loc[:, test.data_raw.columns != 'rating']
-
-    def create_train_split(self):
-        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.x_raw, self.y_raw, test_size=0.3,
-                                                                                random_state=1000)
-    def model_train(self):
-        model.fit(self.x_train, self.y_train)
-
-test = LogicalRegression('../../Data/dataNew.csv')
-test.load_data()
-
-test.conversion_categorical_to_binary()
-test.scaler_columes()
-print(test.data_raw)
-
-test.create_data_for_learning()
-lab = preprocessing.LabelEncoder()
-test.create_train_split()
-
-model.fit(X_train, Y_train)
-y_pred = model.predict(X_raw)
-print(confusion_matrix(Y_raw, y_pred))
-print(log_loss(y_pred, Y_raw))
-print('Б. Всей обучающей выборки')
-y_pred = model.predict(X_train)
-print(confusion_matrix(Y_train, y_pred))
-print(log_loss(y_pred, Y_train))
-print('В. Отобранных на собеседование кандидатов для обучающей выборки')
-print(confusion_matrix(Y_train, y_pred)[1][1])
-print('Г.  Всей тестовой выборки')
-y_pred = model.predict(X_test)
-print(confusion_matrix(Y_test, y_pred))
-print(log_loss(y_pred, Y_test))
-print('Д. Отобранных на собеседование кандидатов для тестовой выборки')
-print(confusion_matrix(Y_test, y_pred)[1][1])
+test.result()
